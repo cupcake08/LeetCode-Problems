079e2d3a726724d4cb5da59d59792b4d3471eb5d
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2021-06-06 11:37:48 +0530

    Update minimum-space-wasted-from-packaging.cpp

--- weeklyContest/contest244/minimum-space-wasted-from-packaging.cpp
+++ weeklyContest/contest244/minimum-space-wasted-from-packaging.cpp
@@ -1,5 +1,29 @@
 //problem link -> https://leetcode.com/problems/minimum-space-wasted-from-packaging/
 //idea -> https://leetcode.com/problems/minimum-space-wasted-from-packaging/discuss/1253948/Python-3-Binary-Search-%2B-pre-sum-explanation-with-picture.
+/*
+Idea:
+Sort packages A,
+Sort boxes[i],
+for each box in box[i] from small to big,
+find out how many packages in A it can covers,
+using binary search.
+
+Assume we alreay cover i packages,
+now we find that we can cover j packages,
+the space we are using (j - i) * box size.
+We accumulate the space size.
+
+No need for prefix sum at all,
+this takes extra unnecessay space.
+The final result is total space - box size sum,
+we just need to calculate the box size sum once,
+and that's enough.
+
+
+Complexity
+Time O(sort(A) + sort(B) + BlogA)
+Space O(sort)
+*/
 int minWastedSpace(vector<int>& packs, vector<vector<int>>& suppliers) {
     sort(begin(packs), end(packs));
     long sum_packs = accumulate(begin(packs), end(packs), 0l), res = LONG_MAX;
